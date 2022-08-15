acb0835a49e7a2f6d9adb18cdd00f81bb3b64550
Faeid.Qarayev <feridqarayev11@gmail.com>
2022-08-15 10:43:08 +0400

    Add project files.

---
+++ Project3/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project3.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
