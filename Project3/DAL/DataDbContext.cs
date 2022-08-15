ce1416f57cc542df1bd452a1a5bce7c424a63475
Faeid.Qarayev <feridqarayev11@gmail.com>
2022-08-15 10:57:50 +0400

    DataBase(Sql) Connection

---
+++ Project3/DAL/DataDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project3.Models;
+
+namespace Project3.DAL
+{
+    public class DataDbContext:DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().ToTable("User");
+        }
+    }
+}
---
+++ Project3/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Project3.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter UserName")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Email")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
